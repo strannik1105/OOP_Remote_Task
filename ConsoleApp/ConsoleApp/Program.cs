3186251df7a30c1484366efe62175c048bf04d4e
strannik1105 <danil.stolpovskikh@gmail.com>
2022-02-11 18:10:48 +0700

    Add Console UI

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -6,7 +6,14 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ввод числа: ");
+            Console.Write("\tЧисло: ");
+            string number = Console.ReadLine();
+            Console.Write("\tСистема счисления: ");
+            int system_base = Int32.Parse(Console.ReadLine());
+            Console.Write("Новая система счисления: ");
+            int new_system_base = Int32.Parse(Console.ReadLine());
+            Console.WriteLine($"Результат: {Number_Systems_Converter.Convert(number, system_base, new_system_base)}");
         }
     }
 }
